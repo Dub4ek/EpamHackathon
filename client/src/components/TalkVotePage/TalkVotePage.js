b1ba24d67c90018343970e75c8f5e789a5f6d20c
Mariia Mironova <mariia_mironova@epam.com>
2019-06-29 00:00:56 +0300

    add vote page items

--- client/src/components/TalkVotePage/TalkVotePage.js
+++ client/src/components/TalkVotePage/TalkVotePage.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import StarIcon from '@material-ui/icons/Star';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 function TalksVotePage() {
+  const classes = useStyles();
+
   return (
-    <main>
-      TalksVotePage
-    </main>
+    <List component="nav" className={classes.root} aria-label="Contacts">
+      <ListItem button>
+        <ListItemIcon>
+          <StarIcon />
+        </ListItemIcon>
+        <ListItemText primary="Текст вопроса" />
+      </ListItem>
+      <ListItem button>
+        <ListItemText inset primary="Текст вопроса" />
+      </ListItem>
+    </List>
   );
 }
 
