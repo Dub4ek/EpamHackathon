3d3ef766fee2fcb7f38e8d7d558757dfb2d6a26a
Andrei_Starodubtcev <andrei_starodubtcev@epam.com>
2019-06-29 11:19:46 +0300

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import LoginPage from "./components/LoginPage/LoginPage";
-import TalksListPage from "./components/TalksListPage/TalksListPage";
-import TalkVotePage from "./components/TalkVotePage/TalkVotePage";
 
 import './App.css';
+import TalksListPage from "./components/TalksListPage/TalksListPage";
+import TalksVotePage from "./components/TalkVotePage/TalkVotePage";
 
 
 function App() {
   return (
     <Router>
-      {/*<Route exact path='/' component={LoginPage}></Route>*/}
-      <Route exact path='/' component={TalksListPage}></Route>
-      {/*<Route exact path='/' component={TalkVotePage}></Route>*/}
+      <Route exact path='/' component={LoginPage}></Route>
+      <Route exact path='/talksList' component={TalksListPage}></Route>
+      <Route exact path='/talkVote' component={TalksVotePage}></Route>
     </Router>
   );
 }
