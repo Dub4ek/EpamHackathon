e0391d095427e3c35996e0a5b4424bb82ef26fe5
Mariia Mironova <mariia_mironova@epam.com>
2019-06-29 12:22:26 +0300

    adds to ui

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import './App.css';
 function App() {
   return (
     <Router>
-      {/*<Route exact path='/' component={LoginPage}></Route>*/}
-      <Route exact path='/' component={TalksListPage}></Route>
+      <Route exact path='/' component={LoginPage}></Route>
+      {/*<Route exact path='/' component={TalksListPage}></Route>*/}
       {/*<Route exact path='/' component={TalkVotePage}></Route>*/}
     </Router>
   );
